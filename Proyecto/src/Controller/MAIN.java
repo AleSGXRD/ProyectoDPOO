64bde904fcef07b735c3ac0d1f7f50309bc53164
DanteXRD <117309248+DanteXRD@users.noreply.github.com>
2022-11-05 00:21:05 -0400

    Añadir Proyecto

---
+++ Proyecto/src/Controller/MAIN.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.Intro;
+
+/**
+ *
+ * @author AleXRD
+ */
+public class MAIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Intro().setVisible(true);
+            }
+        });
+    }
+    
+}
